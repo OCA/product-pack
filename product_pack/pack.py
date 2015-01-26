6d2918b47f8566e6cb8e3c0bda405621b9daf892
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-01-26 10:55:38 -0300

    [IMP/FIX] product pack
    IMP product pack to support surveyor packs
    
    IMP pack description
    
    IMP add product pack standar functionality for v8 and general improvements
    
    IMP discounts and portal distributors permissions
    
    FIX product_pack license and author
    
    FIX prod pack available
    
    IMP pack to restrict company in products
    
    FIX pack company restriction

--- product_pack/pack.py
+++ product_pack/pack.py
@@ -1,109 +1,125 @@
-# -*- encoding: latin-1 -*-
-
-
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import fields, models, api, _
+from openerp.osv import fields as old_fields
+from openerp.exceptions import Warning
 import math
-from openerp.osv import fields,osv
 
-class product_pack( osv.osv ):
+
+class product_pack(models.Model):
     _name = 'product.pack.line'
     _rec_name = 'product_id'
-    _columns = {
-        'parent_product_id': fields.many2one( 'product.product', 'Parent Product', ondelete='cascade', required=True ),
-        'quantity': fields.float( 'Quantity', required=True ),
-        'product_id': fields.many2one( 'product.product', 'Product', required=True ),
-    }
 
-product_pack()
+    parent_product_id = fields.Many2one(
+        'product.product', 'Parent Product',
+        ondelete='cascade', required=True)
+    quantity = fields.Float(
+        'Quantity', required=True)
+    product_id = fields.Many2one(
+        'product.product', 'Product', required=True)
 
-class product_product( osv.osv ):
+
+class product_product(models.Model):
     _inherit = 'product.product'
+
+    pack_line_ids = fields.One2many(
+        'product.pack.line', 'parent_product_id', 'Pack Products',
+        help='List of products that are part of this pack.')
+    used_pack_line_ids = fields.One2many(
+        'product.pack.line', 'product_id', 'Pack Products',
+        help='List of products that are part of this pack.')
+
+    def _product_available(
+            self, cr, uid, ids, field_names=None, arg=False, context=None):
+        pack_product_ids = self.search(cr, uid, [
+            ('pack', '=', True),
+            ('id', 'in', ids),
+        ])
+        res = super(product_product, self)._product_available(
+            cr, uid, list(set(ids) - set(pack_product_ids)),
+            field_names, arg, context)
+        for product in self.browse(cr, uid, pack_product_ids, context=context):
+            pack_qty_available = []
+            pack_virtual_available = []
+            for subproduct in product.pack_line_ids:
+                subproduct_stock = self._product_available(
+                    cr, uid, [subproduct.product_id.id], field_names, arg,
+                    context)[subproduct.product_id.id]
+                sub_qty = subproduct.quantity
+                if sub_qty:
+                    pack_qty_available.append(math.floor(
+                        subproduct_stock['qty_available'] / sub_qty))
+                    pack_virtual_available.append(math.floor(
+                        subproduct_stock['virtual_available'] / sub_qty))
+            # TODO calcular correctamente pack virtual available para negativos
+            res[product.id] = {
+                'qty_available': pack_qty_available and min(pack_qty_available) or False,
+                'incoming_qty': 0,
+                'outgoing_qty': 0,
+                'virtual_available': pack_virtual_available and max(min(pack_virtual_available), 0) or False,
+            }
+        return res
+
+    def _search_product_quantity(self, cr, uid, obj, name, domain, context):
+        return super(product_product, self)._search_product_quantity(
+            cr, uid, obj, name, domain, context)
+
     _columns = {
-        'stock_depends': fields.boolean( 'Stock depends of components', help='Mark if pack stock is calcualted from component stock' ),
-        'pack_fixed_price': fields.boolean( 'Pack has fixed price', help='Mark this field if the public price of the pack should be fixed. Do not mark it if the price should be calculated from the sum of the prices of the products in the pack.' ),
-        'pack_line_ids': fields.one2many( 'product.pack.line','parent_product_id', 'Pack Products', help='List of products that are part of this pack.' ),
+        'qty_available': old_fields.function(
+            _product_available, multi='qty_available',
+            fnct_search=_search_product_quantity),
+        'virtual_available': old_fields.function(
+            _product_available, multi='qty_available',
+            fnct_search=_search_product_quantity),
+        'incoming_qty': old_fields.function(
+            _product_available, multi='qty_available',
+            fnct_search=_search_product_quantity),
+        'outgoing_qty': old_fields.function(
+            _product_available, multi='qty_available',
+            fnct_search=_search_product_quantity),
     }
 
-    def get_product_available( self, cr, uid, ids, context=None ):
-        """ Calulate stock for packs, return  maximum stock that lets complete pack """
-        result={}
-        # for product in self.browse( cr, uid, ids, context=context ):
-        result = super( product_product, self ).get_product_available( cr, uid, ids, context=context )
-        if not isinstance(ids, list):
-            ids = [ids]
-        stock_depends_products = self.search(cr, uid, [('stock_depends', '=',True),('id','in',ids)], context=context) 
-        for product in self.browse( cr, uid, stock_depends_products, context=context ):
+    @api.one
+    @api.constrains('company_id', 'pack_line_ids', 'used_pack_line_ids')
+    def check_pack_line_company(self):
+        # TODO implementar mejores mensajes
+        for line in self.pack_line_ids:
+            if line.product_id.company_id != self.company_id:
+                raise Warning(_(
+                    'Pack lines products company must be the same as the\
+                    parent product company'))
+        for line in self.used_pack_line_ids:
+            if line.parent_product_id.company_id != self.company_id:
+                raise Warning(_(
+                    'Pack lines products company must be the same as the\
+                    parent product company'))
 
-            first_subproduct = True
-            pack_stock = 0
-            # Check if the pack has subproducts
-            if product.pack_line_ids:
-                # Take the stock/virtual stock of all subproducts
-                subproducts_stock = super( product_product, self ).get_product_available(cr, uid, [ line.product_id.id for line in product.pack_line_ids ], context=context )
 
-                # Go over all subproducts, take quantity needed for the pack and its available stock
-                for subproduct in product.pack_line_ids:
-                    if first_subproduct:
-                        subproduct_quantity = subproduct.quantity
-                        subproduct_stock = subproducts_stock[ subproduct.product_id.id ]
-                        # Calculate real stock for current pack from the subproduct stock and needed quantity
-                        pack_stock = math.floor( subproduct_stock / subproduct_quantity )
-                        first_subproduct = False
-                        continue
-                    # Take the info of the next subproduct
-                    subproduct_quantity_next = subproduct.quantity
-                    subproduct_stock_next = subproducts_stock[ subproduct.product_id.id ]
-                    pack_stock_next = math.floor( subproduct_stock_next / subproduct_quantity_next )
-                    # compare the stock of a subproduct and the next subproduct
-                    if pack_stock_next < pack_stock:
-                        pack_stock = pack_stock_next
-                # result is the minimum stock of all subproducts
-                result[ product.id ] = pack_stock
-            else:
-                stock = super( product_product, self ).get_product_available( cr, uid, [ product.id ], context=context )
-                result[ product.id ] = stock[ product.id ]
-        return result
+class product_template(models.Model):
+    _inherit = 'product.template'
 
-# back up de funcion
-    # def get_product_available( self, cr, uid, ids, context=None ):
-    #     """ Calulate stock for packs, return  maximum stock that lets complete pack """
-    #     result={}
-    #     for product in self.browse( cr, uid, ids, context=context ):
-    #         stock = super( product_product, self ).get_product_available( cr, uid, [ product.id ], context=context )
-    #         print product.id
-    #         # Check if product stock depends on it's subproducts stock.
-    #         if not product.stock_depends:
-    #             result[ product.id ] = stock[ product.id ]
-    #             continue
-
-    #         first_subproduct = True
-    #         pack_stock = 0
-
-    #         # Check if the pack has subproducts
-    #         if product.pack_line_ids:
-    #             # Take the stock/virtual stock of all subproducts
-    #             subproducts_stock = self.get_product_available( cr, uid, [ line.product_id.id for line in product.pack_line_ids ], context=context )
-    #             # Go over all subproducts, take quantity needed for the pack and its available stock
-    #             for subproduct in product.pack_line_ids:
-    #                 if first_subproduct:
-    #                     subproduct_quantity = subproduct.quantity
-    #                     subproduct_stock = subproducts_stock[ subproduct.product_id.id ]
-    #                     # Calculate real stock for current pack from the subproduct stock and needed quantity
-    #                     pack_stock = math.floor( subproduct_stock / subproduct_quantity )
-    #                     first_subproduct = False
-    #                     continue
-    #                 # Take the info of the next subproduct
-    #                 subproduct_quantity_next = subproduct.quantity
-    #                 subproduct_stock_next = subproducts_stock[ subproduct.product_id.id ]
-    #                 pack_stock_next = math.floor( subproduct_stock_next / subproduct_quantity_next )
-    #                 # compare the stock of a subproduct and the next subproduct
-    #                 if pack_stock_next < pack_stock:
-    #                     pack_stock = pack_stock_next
-    #             # result is the minimum stock of all subproducts
-    #             result[ product.id ] = pack_stock
-    #         else:
-    #             result[ product.id ] = stock[ product.id ]
-    #     return result
-
-product_product()
+    pack_price_type = fields.Selection([
+        ('components_price', 'Components Prices'),
+        # TODO modify price_get and add this functionality
+        # ('totalice_price', 'Totalice Price'),
+        ('fixed_price', 'Fixed Price'),
+    ],
+        'Pack Price Type',
+        help="""
+        * Totalice Price: Sum individual prices on the product pack price.
+        * Fixed Price: Price of this product instead of components prrices.
+        * Components Price: Components prices plast pack price.
+        """
+    )
+    sale_order_pack = fields.Boolean(
+        'Sale Order Pack',
+        help='Sale order are packs used on sale orders to calculate a price of a line',
+    )
+    pack = fields.Boolean(
+        'Pack?',
+        help='TODO',
+    )
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
