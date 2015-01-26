36f2c92469c8b8c2875b948be72cfe66bc9f4bc8
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-01-26 10:55:38 -0300

    [IMP/FIX] product pack
    IMP product pack to support surveyor packs
    
    IMP pack description
    
    IMP add product pack standar functionality for v8 and general improvements
    
    IMP discounts and portal distributors permissions
    
    FIX product_pack license and author
    
    FIX prod pack available
    
    IMP pack to restrict company in products
    
    FIX pack company restriction

--- product_pack/__openerp__.py
+++ product_pack/__openerp__.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
-
-
+##############################################################################
+#
+#    Copyright (C) 2009  Àngel Àlvarez - NaN  (http://www.nan-tic.com)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     'name': 'Product Pack',
     'version': '1.0',
@@ -10,27 +27,28 @@
     'description': """
 Product Pack
 ============
-Allows configuring products as a collection of other products. If such a product is added in a sale order, all the products of the pack will be added automatically (when storing the order) as children of the pack product.
-
-The module has been made compatible with nan_external_prices and containts code to specifically handle when the module is available but they're still independent and there are no dependencies between them.
+# TODO agregar en configuracion si se quiere usar los sale order packs (seria para el group group_pack) y ver que se haga visible la vista form
+# TODO implementar totalice en price get
+# TODO agregar constraint de no pack dentro de pack
+# TODO calcular correctamente pack virtual available para negativos
     """,
-    'author':  'Ingenieria ADHOC',
-    'website': 'www.ingadhoc.com',
+    'author':  'NaN·tic, ADHOC',
     'images': [
     ],
     'depends': [
         'sale',
-        'stock',
     ],
     'data': [
-		'security/ir.model.access.csv',
-		'pack_view.xml'
+        'security/ir.model.access.csv',
+        'security/product_security.xml',
+        'pack_view.xml',
+        'sale_view.xml',
     ],
     'demo': [
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
