a4f06ad5e789d54cd2e118377f74f4d9c8cff0c4
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-23 13:33:21 -0300

    [IMP/FIX] product_pack
    IMP product_pack format
    
    FIX fix_digits_compute_to_digits
    
    IMP IMPORTANT imprrovements on product pack, get pack with sale order constraint
    
    IMP use constraint on sale line and not sale order for product packs
    
    IMP implement totalice price on product pack
    
    IMP update pack manifest
    
    FIX prod pack copy sale order
    
    FIX contraint
    
    IMP compt with other modules
    
    FIX prod pack @api.multi

---
+++ product_pack/models/sale_order.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.one
+    def copy(self, default=None):
+        sale_copy = super(SaleOrder, self).copy(default)
+        # we unlink pack lines that should not be copied
+        pack_copied_lines = sale_copy.order_line.filtered(
+            lambda l: l.pack_parent_line_id.order_id == self)
+        pack_copied_lines.unlink()
+        return sale_copy
