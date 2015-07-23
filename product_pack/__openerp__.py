78996be873ad4a31a1faa81d3a25a9d53aba03d9
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-23 13:33:21 -0300

    [IMP/FIX] product_pack
    IMP product_pack format
    
    FIX fix_digits_compute_to_digits
    
    IMP IMPORTANT imprrovements on product pack, get pack with sale order constraint
    
    IMP use constraint on sale line and not sale order for product packs
    
    IMP implement totalice price on product pack
    
    IMP update pack manifest
    
    FIX prod pack copy sale order
    
    FIX contraint
    
    IMP compt with other modules
    
    FIX prod pack @api.multi

--- product_pack/__openerp__.py
+++ product_pack/__openerp__.py
@@ -20,19 +20,12 @@
 ##############################################################################
 {
     'name': 'Product Pack',
-    'version': '1.0',
+    'version': '9.0.1.1.0',
     'category': 'Product',
     'sequence': 14,
     'summary': '',
-    'description': """
-Product Pack
-============
-# TODO agregar en configuracion si se quiere usar los sale order packs (seria para el group group_pack) y ver que se haga visible la vista form
-# TODO implementar totalice en price get
-# TODO agregar constraint de no pack dentro de pack
-# TODO calcular correctamente pack virtual available para negativos
-    """,
-    'author':  'NaN·tic, ADHOC',
+    'author': 'NaN·tic, ADHOC SA',
+    'license': 'AGPL-3',
     'images': [
     ],
     'depends': [
@@ -40,11 +33,11 @@ Product Pack
     ],
     'data': [
         'security/ir.model.access.csv',
-        'security/product_security.xml',
-        'pack_view.xml',
-        'sale_view.xml',
+        'views/pack_view.xml',
+        'views/sale_view.xml',
     ],
     'demo': [
+        'demo/demo_data.xml',
     ],
     'test': [
     ],
@@ -52,4 +45,3 @@ Product Pack
     'auto_install': False,
     'application': False,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
