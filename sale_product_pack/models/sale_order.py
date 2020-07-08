d40a5f5db2a29b1c0877b1d33b77e7acac6cc81b
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-07-08 13:44:36 -0400

    [MIG] sale_product_pack: Migration to 13.0 (fix order line deleting)

--- sale_product_pack/models/sale_order.py
+++ sale_product_pack/models/sale_order.py
@@ -37,3 +37,23 @@ class SaleOrder(models.Model):
                     " delete the pack itself"
                 )
             )
+
+    def write(self, vals):
+        if "order_line" in vals:
+            to_delete_ids = [e[1] for e in vals["order_line"] if e[0] == 2]
+            subpacks_to_delete_ids = (
+                self.env["sale.order.line"]
+                .search(
+                    [("id", "child_of", to_delete_ids), ("id", "not in", to_delete_ids)]
+                )
+                .ids
+            )
+            if subpacks_to_delete_ids:
+                for cmd in vals["order_line"]:
+                    if cmd[1] in subpacks_to_delete_ids:
+                        if cmd[0] != 2:
+                            cmd[0] = 2
+                        subpacks_to_delete_ids.remove(cmd[1])
+                for to_delete_id in subpacks_to_delete_ids:
+                    vals["order_line"].append([2, to_delete_id, False])
+        return super().write(vals)
