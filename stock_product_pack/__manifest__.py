92ce91e952f49687f517578067d126e1e6fee142
João Marques <joao.marques@tecnativa.com>
2020-09-21 15:08:42 +0200

    [MIG] stock_product_pack: Migration to 13.0

--- stock_product_pack/__manifest__.py
+++ stock_product_pack/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2019 NaN (http://www.nan-tic.com) - Àngel Àlvarez
 # Copyright 2019 Tecnativa - Ernesto Tejeda
+# Copyright 2020 Tecnativa - João Marques
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock product Pack",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "category": "Warehouse",
     "summary": "This module allows you to get the right available quantities "
     "of the packs",
@@ -11,7 +12,7 @@
     "author": "NaN·tic, " "ADHOC SA, " "Tecnativa, " "Odoo Community Association (OCA)",
     "maintainers": ["ernestotejeda"],
     "license": "AGPL-3",
-    "depends": ["product_pack", "stock",],
+    "depends": ["product_pack", "stock"],
     "installable": True,
     "auto_install": True,
     "application": False,
