0679726a0e0aff6865031dbbcf0af577bc990bb8
david <david.vidal@tecnativa.com>
2021-06-18 17:19:06 +0200

    [ADD] website_sale_product_pack: New module
    Compatibility module between sale_product_pack and website_sale
    
    TT30385

---
+++ website_sale_product_pack/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Product Pack",
+    "category": "E-Commerce",
+    "summary": "Compatibility module of product pack with e-commerce",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["website_sale", "sale_product_pack"],
+    "data": ["views/assets.xml", "views/templates.xml"],
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-pack",
+    "installable": True,
+}
