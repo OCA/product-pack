426850705131b5c509f1ee84bed4e209592deacd
david <david.vidal@tecnativa.com>
2021-06-18 17:19:06 +0200

    [ADD] website_sale_product_pack: New module
    Compatibility module between sale_product_pack and website_sale
    
    TT30385

---
+++ website_sale_product_pack/static/src/js/website_sale_product_pack_tour.js
@@ -0,0 +1,160 @@
+/* Copyright 2021 Tecnativa - David Vidal
+ * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
+
+odoo.define(
+    "website_sale_product_pack.tour_create_components_price_order_line",
+    function(require) {
+        "use strict";
+
+        var tour = require("web_tour.tour");
+
+        var steps = [
+            {
+                trigger:
+                    "a:contains('Pack CPU (Detailed - Displayed Components Price)')",
+            },
+            {
+                trigger: "a:contains('Add to Cart')",
+            },
+            {
+                trigger: "a:contains('Process Checkout')",
+            },
+            {
+                trigger: "a[href='/shop']",
+            },
+        ];
+        tour.register(
+            "create_components_price_order_line",
+            {
+                url: "/shop",
+                test: true,
+            },
+            steps
+        );
+    }
+);
+
+odoo.define("website_sale_product_pack.tour_create_ignored_price_order_line", function(
+    require
+) {
+    "use strict";
+
+    var tour = require("web_tour.tour");
+
+    var steps = [
+        {
+            trigger: "a:contains('Pack CPU (Detailed - Ignored Components Price)')",
+        },
+        {
+            trigger: "a:contains('Add to Cart')",
+        },
+        {
+            trigger: "a:contains('Process Checkout')",
+        },
+        {
+            trigger: "a[href='/shop']",
+        },
+    ];
+    tour.register(
+        "create_ignored_price_order_line",
+        {
+            url: "/shop",
+            test: true,
+        },
+        steps
+    );
+});
+
+odoo.define(
+    "website_sale_product_pack.tour_create_totalized_price_order_line",
+    function(require) {
+        "use strict";
+
+        var tour = require("web_tour.tour");
+
+        var steps = [
+            {
+                trigger:
+                    "a:contains('Pack CPU (Detailed - Totalized Components Price)')",
+            },
+            {
+                trigger: "a:contains('Add to Cart')",
+            },
+            {
+                trigger: "a:contains('Process Checkout')",
+            },
+            {
+                trigger: "a[href='/shop']",
+            },
+        ];
+        tour.register(
+            "create_totalized_price_order_line",
+            {
+                url: "/shop",
+                test: true,
+            },
+            steps
+        );
+    }
+);
+
+odoo.define(
+    "website_sale_product_pack.tour_create_non_detailed_price_order_line",
+    function(require) {
+        "use strict";
+
+        var tour = require("web_tour.tour");
+
+        var steps = [
+            {
+                trigger: "a:contains('Non Detailed - Totalized Components Price')",
+            },
+            {
+                trigger: "a:contains('Add to Cart')",
+            },
+            {
+                trigger: "a:contains('Process Checkout')",
+            },
+            {
+                trigger: "a[href='/shop']",
+            },
+        ];
+        tour.register(
+            "create_non_detailed_price_order_line",
+            {
+                url: "/shop",
+                test: true,
+            },
+            steps
+        );
+    }
+);
+
+odoo.define("website_sale_product_pack.tour_update_pack_qty", function(require) {
+    "use strict";
+
+    var tour = require("web_tour.tour");
+
+    var steps = [
+        {
+            trigger: "a:contains('Pack CPU (Detailed - Displayed Components Price)')",
+        },
+        {
+            trigger: "a:contains('Add to Cart')",
+        },
+        {
+            trigger: "a:contains('Process Checkout')",
+        },
+        {
+            trigger: "a[href='/shop']",
+        },
+    ];
+    tour.register(
+        "update_pack_qty",
+        {
+            url: "/shop",
+            test: true,
+        },
+        steps
+    );
+});
