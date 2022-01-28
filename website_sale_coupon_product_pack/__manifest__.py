3181ed365586b17a887c398b7db578a333410b1d
jinye <jinye@domatix.com>
2022-01-28 12:48:21 +0100

    website_sale_coupon_product_pack: [FIX] Fixed duplicated reward line image

---
+++ website_sale_coupon_product_pack/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Domatix - Jinye Ji
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Coupon Product Pack",
+    "category": "E-Commerce",
+    "summary": "Compatibility module of website sale product pack with website sale coupon",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["website_sale_coupon", "website_sale_product_pack"],
+    "data": ["views/templates.xml"],
+    "author": "Domatix, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-pack",
+    "installable": True,
+}
---
+++ website_sale_coupon_product_pack/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module introduces compatibility of product packs with website sale coupon:
+
+- In the cart summary, the image of a reward line doesn´t appear duplicate.
