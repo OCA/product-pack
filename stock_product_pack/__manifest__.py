0ca10c65c2c07094a43de2123dd5e5a56b6bd228
Achraf Mhadhbi <machraf@bloopark.de>
2022-06-28 17:05:41 +0200

    [IMP] stock_product_pack: black, isort, prettier

--- stock_product_pack/__manifest__.py
+++ stock_product_pack/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock product Pack",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.1",
     "category": "Warehouse",
     "summary": "This module allows you to get the right available quantities "
     "of the packs",
