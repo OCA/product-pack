18d8e3d2c7757684c5d26772c150faca79ce2efc
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-07-05 21:02:39 +0200

    Update dotfiles + switch to GH actions
    sale_stock_product_pack 13.0.1.0.2
    
    [UPD] README.rst

--- sale_stock_product_pack/__manifest__.py
+++ sale_stock_product_pack/__manifest__.py
@@ -3,10 +3,10 @@
 {
     "name": "Sale Stock Product Pack",
     "summary": "Compatibility module for packs that are storable products",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "development_status": "Beta",
     "category": "Sale",
-    "website": "https://github.com/OCA/product-pack.git",
+    "website": "https://github.com/OCA/product-pack",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
