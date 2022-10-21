9521d18234f3b3a21a4172da6f419c2dd4769970
augusto-weiss <awe@adhoc.com.ar>
2022-10-21 16:53:05 -0300

    [WIP][FIX] product_pricelist: pricelist based on pricelist with diferent currency

--- product_pack/models/product_pricelist.py
+++ product_pack/models/product_pricelist.py
@@ -1,7 +1,8 @@
 # Copyright 2022 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from itertools import chain
 
-from odoo import models
+from odoo import fields, models
 
 
 class ProductPricelist(models.Model):
@@ -12,6 +13,54 @@ class ProductPricelist(models.Model):
         as the computations done after calling `price_compute` modify the final returned
         price, so we compute it directly in these cases.
         """
+
+        if not date:
+            date = self._context.get("date") or fields.Datetime.now()
+        if not uom_id and self._context.get("uom"):
+            uom_id = self._context["uom"]
+        if uom_id:
+            # rebrowse with uom if given
+            products = [
+                item[0].with_context(uom=uom_id) for item in products_qty_partner
+            ]
+            products_qty_partner = [
+                (products[index], data_struct[1], data_struct[2])
+                for index, data_struct in enumerate(products_qty_partner)
+            ]
+        else:
+            products = [item[0] for item in products_qty_partner]
+        if not products:
+            return {}
+        categ_ids = {}
+        for p in products:
+            categ = p.categ_id
+            while categ:
+                categ_ids[categ.id] = True
+                categ = categ.parent_id
+        categ_ids = list(categ_ids)
+        is_product_template = products[0]._name == "product.template"
+        if is_product_template:
+            prod_tmpl_ids = [tmpl.id for tmpl in products]
+            # all variants of all products
+            prod_ids = [
+                p.id
+                for p in list(
+                    chain.from_iterable([t.product_variant_ids for t in products])
+                )
+            ]
+        else:
+            prod_ids = [product.id for product in products]
+            prod_tmpl_ids = [product.product_tmpl_id.id for product in products]
+
+        items = self._compute_price_rule_get_items(
+            products_qty_partner, date, uom_id, prod_tmpl_ids, prod_ids, categ_ids
+        )
+        for rule in items:
+            if rule.base == "pricelist" and rule.base_pricelist_id:
+                return super()._compute_price_rule(
+                    products_qty_partner, date=date, uom_id=uom_id
+                )
+
         products_qty_partner_super = [
             (s[0], s[1], s[2])
             for s in products_qty_partner
