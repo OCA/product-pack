9ddf293a6b566d385ced60c678f09f7c86f6fc7e
Phillip Siessl <phsi@odoovm1>
2023-01-09 15:18:17 +0100

    [MIG] stock_product_pack: Migration to 15.0

--- sale_product_pack/tests/test_sale_product_pack.py
+++ sale_product_pack/tests/test_sale_product_pack.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import SavepointCase
+from odoo.tests import TransactionCase
 
 
 class TestSaleProductPack(TransactionCase):
