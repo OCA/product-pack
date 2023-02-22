26ef2dee0b22c5f87643e09ff33bc37741ed8286
Maxime Franco <maxime.franco@acsone.eu>
2023-02-22 08:46:45 +0100

    [MIG] sale_product_pack: Migration to 16.0

--- sale_product_pack/tests/test_sale_product_pack.py
+++ sale_product_pack/tests/test_sale_product_pack.py
@@ -1,10 +1,10 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestSaleProductPack(SavepointCase):
+class TestSaleProductPack(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
