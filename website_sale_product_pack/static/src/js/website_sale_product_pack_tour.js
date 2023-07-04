820f59ec83406b44c5f22dc33ab4469ed3c5b90f
alvaro <alvaro@domatix.com>
2023-07-04 13:13:07 +0200

    [16.0][MIG] website_sale_product_pack: Migration to 16.0

--- website_sale_product_pack/static/src/js/website_sale_product_pack_tour.js
+++ website_sale_product_pack/static/src/js/website_sale_product_pack_tour.js
@@ -3,10 +3,11 @@
 
 odoo.define(
     "website_sale_product_pack.tour_create_components_price_order_line",
-    function(require) {
+    function (require) {
         "use strict";
 
         var tour = require("web_tour.tour");
+        const tourUtils = require("website_sale.tour_utils");
 
         var steps = [
             {
@@ -14,10 +15,16 @@ odoo.define(
                     "a:contains('Pack CPU (Detailed - Displayed Components Price)')",
             },
             {
-                trigger: "a:contains('Add to Cart')",
+                trigger: "#add_to_cart",
+            },
+
+            tourUtils.goToCart({quantity: 1}),
+
+            {
+                trigger: "a[href='/shop/cart']",
             },
             {
-                trigger: "a:contains('Process Checkout')",
+                trigger: "a.btn-primary:contains('Process Checkout')",
             },
             {
                 trigger: "a[href='/shop']",
@@ -34,54 +41,67 @@ odoo.define(
     }
 );
 
-odoo.define("website_sale_product_pack.tour_create_ignored_price_order_line", function(
-    require
-) {
-    "use strict";
-
-    var tour = require("web_tour.tour");
-
-    var steps = [
-        {
-            trigger: "a:contains('Pack CPU (Detailed - Ignored Components Price)')",
-        },
-        {
-            trigger: "a:contains('Add to Cart')",
-        },
-        {
-            trigger: "a:contains('Process Checkout')",
-        },
-        {
-            trigger: "a[href='/shop']",
-        },
-    ];
-    tour.register(
-        "create_ignored_price_order_line",
-        {
-            url: "/shop",
-            test: true,
-        },
-        steps
-    );
-});
-
 odoo.define(
-    "website_sale_product_pack.tour_create_totalized_price_order_line",
-    function(require) {
+    "website_sale_product_pack.tour_create_ignored_price_order_line",
+    function (require) {
         "use strict";
 
         var tour = require("web_tour.tour");
+        const tourUtils = require("website_sale.tour_utils");
+        var steps = [
+            {
+                trigger: "a:contains('Pack CPU (Detailed - Ignored Components Price)')",
+            },
+            {
+                trigger: "#add_to_cart",
+            },
 
+            tourUtils.goToCart({quantity: 1}),
+
+            {
+                trigger: "a[href='/shop/cart']",
+            },
+            {
+                trigger: "a.btn-primary:contains('Process Checkout')",
+            },
+            {
+                trigger: "a[href='/shop']",
+            },
+        ];
+        tour.register(
+            "create_ignored_price_order_line",
+            {
+                url: "/shop",
+                test: true,
+            },
+            steps
+        );
+    }
+);
+
+odoo.define(
+    "website_sale_product_pack.tour_create_totalized_price_order_line",
+    function (require) {
+        "use strict";
+
+        var tour = require("web_tour.tour");
+        const tourUtils = require("website_sale.tour_utils");
         var steps = [
             {
                 trigger:
                     "a:contains('Pack CPU (Detailed - Totalized Components Price)')",
             },
             {
-                trigger: "a:contains('Add to Cart')",
+                trigger: "#add_to_cart",
+            },
+
+            tourUtils.goToCart({quantity: 1}),
+
+            {
+                trigger: "a[href='/shop/cart']",
             },
             {
-                trigger: "a:contains('Process Checkout')",
+                trigger: "a.btn-primary:contains('Process Checkout')",
             },
             {
                 trigger: "a[href='/shop']",
@@ -100,20 +120,26 @@ odoo.define(
 
 odoo.define(
     "website_sale_product_pack.tour_create_non_detailed_price_order_line",
-    function(require) {
+    function (require) {
         "use strict";
 
         var tour = require("web_tour.tour");
-
+        const tourUtils = require("website_sale.tour_utils");
         var steps = [
             {
                 trigger: "a:contains('Non Detailed - Totalized Components Price')",
             },
             {
-                trigger: "a:contains('Add to Cart')",
+                trigger: "#add_to_cart",
+            },
+
+            tourUtils.goToCart({quantity: 1}),
+
+            {
+                trigger: "a[href='/shop/cart']",
             },
             {
-                trigger: "a:contains('Process Checkout')",
+                trigger: "a.btn-primary:contains('Process Checkout')",
             },
             {
                 trigger: "a[href='/shop']",
@@ -130,20 +156,23 @@ odoo.define(
     }
 );
 
-odoo.define("website_sale_product_pack.tour_update_pack_qty", function(require) {
+odoo.define("website_sale_product_pack.tour_update_pack_qty", function (require) {
     "use strict";
 
     var tour = require("web_tour.tour");
-
+    const tourUtils = require("website_sale.tour_utils");
     var steps = [
         {
             trigger: "a:contains('Pack CPU (Detailed - Displayed Components Price)')",
         },
         {
-            trigger: "a:contains('Add to Cart')",
+            trigger: "#add_to_cart",
         },
+
+        tourUtils.goToCart({quantity: 1}),
+
         {
-            trigger: "a:contains('Process Checkout')",
+            trigger: "a.btn-primary:contains('Process Checkout')",
         },
         {
             trigger: "a[href='/shop']",
