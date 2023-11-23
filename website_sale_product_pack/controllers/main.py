363601776bcf7dcc259cb5321de3721dae243d16
augusto-weiss <awe@adhoc.com.ar>
2023-11-23 15:49:59 -0300

    [FIX] website_product_pack: detailed displayed components price on website

---
+++ website_sale_product_pack/controllers/main.py
@@ -0,0 +1,26 @@
+from odoo.http import request
+
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSale(WebsiteSale):
+    def shop(
+        self,
+        page=0,
+        category=None,
+        search="",
+        min_price=0.0,
+        max_price=0.0,
+        ppg=False,
+        **post,
+    ):
+        request.update_context(whole_pack_price=True)
+        return super().shop(
+            page=page,
+            category=category,
+            search=search,
+            min_price=min_price,
+            max_price=max_price,
+            ppg=ppg,
+            **post,
+        )
