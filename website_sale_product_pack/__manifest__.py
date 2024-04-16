7ae496ada14d93745f19c30401114c1a7c1666fc
Matias Peralta <matias_peralta1@hotmail.com>
2024-04-16 18:27:43 -0300

    [FIX]website_sale_product_pack: element not founded

--- website_sale_product_pack/__manifest__.py
+++ website_sale_product_pack/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Website Sale Product Pack",
     "category": "E-Commerce",
     "summary": "Compatibility module of product pack with e-commerce",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "license": "AGPL-3",
     "depends": ["website_sale", "sale_product_pack"],
     "data": ["views/templates.xml"],
