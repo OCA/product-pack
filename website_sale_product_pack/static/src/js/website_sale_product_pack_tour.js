763fa4dff8d24527d8c8a76a86f4ef8a577a996e
Mikheil Barnaveli <barnaveli@hmg.software>
2024-08-27 15:42:56 +0400

    [IMP] website_sale_product_pack: black, isort, prettier

--- website_sale_product_pack/static/src/js/website_sale_product_pack_tour.js
+++ website_sale_product_pack/static/src/js/website_sale_product_pack_tour.js
@@ -3,7 +3,7 @@
 
 odoo.define(
     "website_sale_product_pack.tour_create_components_price_order_line",
-    function(require) {
+    function (require) {
         "use strict";
 
         var tour = require("web_tour.tour");
@@ -34,40 +34,41 @@ odoo.define(
     }
 );
 
-odoo.define("website_sale_product_pack.tour_create_ignored_price_order_line", function(
-    require
-) {
-    "use strict";
+odoo.define(
+    "website_sale_product_pack.tour_create_ignored_price_order_line",
+    function (require) {
+        "use strict";
 
-    var tour = require("web_tour.tour");
+        var tour = require("web_tour.tour");
 
-    var steps = [
-        {
-            trigger: "a:contains('Pack CPU (Detailed - Ignored Components Price)')",
-        },
-        {
-            trigger: "a:contains('Add to Cart')",
-        },
-        {
-            trigger: "a:contains('Process Checkout')",
-        },
-        {
-            trigger: "a[href='/shop']",
-        },
-    ];
-    tour.register(
-        "create_ignored_price_order_line",
-        {
-            url: "/shop",
-            test: true,
-        },
-        steps
-    );
-});
+        var steps = [
+            {
+                trigger: "a:contains('Pack CPU (Detailed - Ignored Components Price)')",
+            },
+            {
+                trigger: "a:contains('Add to Cart')",
+            },
+            {
+                trigger: "a:contains('Process Checkout')",
+            },
+            {
+                trigger: "a[href='/shop']",
+            },
+        ];
+        tour.register(
+            "create_ignored_price_order_line",
+            {
+                url: "/shop",
+                test: true,
+            },
+            steps
+        );
+    }
+);
 
 odoo.define(
     "website_sale_product_pack.tour_create_totalized_price_order_line",
-    function(require) {
+    function (require) {
         "use strict";
 
         var tour = require("web_tour.tour");
@@ -100,7 +101,7 @@ odoo.define(
 
 odoo.define(
     "website_sale_product_pack.tour_create_non_detailed_price_order_line",
-    function(require) {
+    function (require) {
         "use strict";
 
         var tour = require("web_tour.tour");
@@ -130,7 +131,7 @@ odoo.define(
     }
 );
 
-odoo.define("website_sale_product_pack.tour_update_pack_qty", function(require) {
+odoo.define("website_sale_product_pack.tour_update_pack_qty", function (require) {
     "use strict";
 
     var tour = require("web_tour.tour");
