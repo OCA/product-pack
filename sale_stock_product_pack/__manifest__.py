040a1be839cdf5e3a5a1fc075b21dac7c260b97b
hyperjump888 <111408659+hyperjump888@users.noreply.github.com>
2025-09-25 14:46:37 +0700

    support for Odoo 19

--- sale_stock_product_pack/__manifest__.py
+++ sale_stock_product_pack/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Stock Product Pack",
     "summary": "Compatibility module for packs that are storable products",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.1",
     "development_status": "Beta",
     "category": "Sale",
     "website": "https://github.com/OCA/product-pack",
