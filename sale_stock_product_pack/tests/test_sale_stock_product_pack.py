0f97530559ec761d85ad99447320b192381416ba
Víctor Martínez <victor.martinez@tecnativa.com>
2026-06-10 17:51:38 +0200

    [IMP+FIX] sale_stock_product_pack + stock_product_pack: Display Delivery Slip data correctly sorted in pack products
    Related to https://github.com/OCA/product-pack/pull/182/changes/63e445ac02276bcbd2cd44a74e1457b5cb4c05cf
    
    Changes done:
    - The _get_aggregated_product_quantities() method has been simplified; if there are any "pack moves", the stock.move.line records are sorted according to the stock.move order so that the data appears in the correct order
    - The _get_aggregated_product_quantities() method has been moved to stock_product_pack so that it works in all cases (sales and purchases)
    - The _get_aggregated_properties() method is overridden to specify a different line_key, ensuring that data is not incorrectly grouped if there are extra lines using any of the products contained in the pack
    - Tests have been added to cover more complex use cases (component1 + component2 + product_pack) data
    
    TT62930

--- sale_stock_product_pack/tests/test_sale_stock_product_pack.py
+++ sale_stock_product_pack/tests/test_sale_stock_product_pack.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Tecnativa - David Vidal
-# Copyright 2024 Tecnativa - Víctor Martínez
+# Copyright 2024-2026 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import Command
 from odoo.tests import Form
 
 from odoo.addons.base.tests.common import BaseCommon
@@ -38,26 +39,23 @@ class TestSaleStockProductPack(BaseCommon):
                 "pack_component_price": "detailed",
                 "categ_id": cls.category_all.id,
                 "pack_line_ids": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {"product_id": cls.component_1.id, "quantity": 1},
                     ),
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {"product_id": cls.component_2.id, "quantity": 1},
                     ),
                 ],
             }
         )
 
-    def _create_sale_order(self, product, qty):
+    def _create_sale_order(self, lines):
         sale_form = Form(self.env["sale.order"])
         sale_form.partner_id = self.partner
-        with sale_form.order_line.new() as line:
-            line.product_id = product
-            line.product_uom_qty = qty
+        for line_data in lines:
+            with sale_form.order_line.new() as line:
+                line.product_id = line_data[0]
+                line.product_uom_qty = line_data[1]
         return sale_form.save()
 
     def _create_stock_quant(self, product, qty):
@@ -73,9 +71,8 @@ class TestSaleStockProductPack(BaseCommon):
         self.product_pack.type = "product"
         self.product_pack.invoice_policy = "delivery"
         self.product_pack.pack_line_ids.product_id.invoice_policy = "delivery"
-        sale = self._create_sale_order(self.product_pack, 9)
+        sale = self._create_sale_order([(self.product_pack, 9)])
         sale.action_confirm()
-
         pack_line = sale.order_line.filtered(
             lambda x: x.product_id == self.product_pack
         )
@@ -91,8 +88,22 @@ class TestSaleStockProductPack(BaseCommon):
         sale.order_line.mapped("qty_delivered")
         self.assertEqual(9, pack_line.qty_delivered)
 
-    def test_picking_pack_consu(self):
-        sale = self._create_sale_order(self.product_pack, 1)
+    def _get_aggregated_product_quantities(self, sol):
+        sol_data = sol.move_ids.move_line_ids._get_aggregated_product_quantities()
+        key_0 = list(sol_data.keys())[0]
+        return sol_data[key_0]
+
+    def test_picking_pack_consu_01(self):
+        sale = self._create_sale_order([(self.product_pack, 1)])
+        sol_product_pack = sale.order_line.filtered(
+            lambda x: x.product_id == self.product_pack
+        )
+        sol_component_1 = sale.order_line.filtered(
+            lambda x: x.product_id == self.component_1
+        )
+        sol_component_2 = sale.order_line.filtered(
+            lambda x: x.product_id == self.component_2
+        )
         self._create_stock_quant(self.component_1, 1)
         self._create_stock_quant(self.component_2, 1)
         sale.action_confirm()
@@ -108,3 +119,79 @@ class TestSaleStockProductPack(BaseCommon):
         self.assertEqual(
             data_names, ["Pack (consumable)", "Component 1", "Component 2"]
         )
+        line_product_pack_data = self._get_aggregated_product_quantities(
+            sol_product_pack
+        )
+        self.assertEqual(line_product_pack_data["qty_ordered"], 1)
+        self.assertEqual(line_product_pack_data["qty_done"], 1)
+        line_component_1_data = self._get_aggregated_product_quantities(sol_component_1)
+        self.assertEqual(line_component_1_data["qty_ordered"], 1)
+        self.assertEqual(line_component_1_data["qty_done"], 1)
+        line_component_2_data = self._get_aggregated_product_quantities(sol_component_2)
+        self.assertEqual(line_component_2_data["qty_ordered"], 1)
+        self.assertEqual(line_component_2_data["qty_done"], 1)
+
+    def test_picking_pack_consu_02(self):
+        sale = self._create_sale_order(
+            [(self.component_1, 1), (self.component_2, 10), (self.product_pack, 2)]
+        )
+        sol_component_1 = sale.order_line.filtered(
+            lambda x: x.product_id == self.component_1 and not x.pack_parent_line_id
+        )
+        sol_component_2 = sale.order_line.filtered(
+            lambda x: x.product_id == self.component_2 and not x.pack_parent_line_id
+        )
+        sol_product_pack = sale.order_line.filtered(
+            lambda x: x.product_id == self.product_pack
+        )
+        sol_product_pack_component_1 = sale.order_line.filtered(
+            lambda x: x.pack_parent_line_id == sol_product_pack
+            and x.product_id == self.component_1
+        )
+        sol_product_pack_component_2 = sale.order_line.filtered(
+            lambda x: x.pack_parent_line_id == sol_product_pack
+            and x.product_id == self.component_2
+        )
+        self._create_stock_quant(self.component_1, 3)
+        self._create_stock_quant(self.component_2, 12)
+        sale.action_confirm()
+        picking = sale.picking_ids
+        res = picking.button_validate()
+        wizard = self.env[res["res_model"]].with_context(**res["context"]).create({})
+        wizard.process()
+        self.assertEqual(picking.state, "done")
+        data_names = []
+        aggregated_lines = picking.move_line_ids._get_aggregated_product_quantities()
+        for line in aggregated_lines:
+            data_names.append(aggregated_lines[line]["name"])
+        self.assertEqual(
+            data_names,
+            [
+                "Component 1",
+                "Component 2",
+                "Pack (consumable)",
+                "Component 1",
+                "Component 2",
+            ],
+        )
+        line_component_1_data = self._get_aggregated_product_quantities(sol_component_1)
+        self.assertEqual(line_component_1_data["qty_ordered"], 1)
+        self.assertEqual(line_component_1_data["qty_done"], 1)
+        line_component_2_data = self._get_aggregated_product_quantities(sol_component_2)
+        self.assertEqual(line_component_2_data["qty_ordered"], 10)
+        self.assertEqual(line_component_2_data["qty_done"], 10)
+        line_product_pack_data = self._get_aggregated_product_quantities(
+            sol_product_pack
+        )
+        self.assertEqual(line_product_pack_data["qty_ordered"], 2)
+        self.assertEqual(line_product_pack_data["qty_done"], 2)
+        line_product_pack_component_1_data = self._get_aggregated_product_quantities(
+            sol_product_pack_component_1
+        )
+        self.assertEqual(line_product_pack_component_1_data["qty_ordered"], 2)
+        self.assertEqual(line_product_pack_component_1_data["qty_done"], 2)
+        line_product_pack_component_2_data = self._get_aggregated_product_quantities(
+            sol_product_pack_component_2
+        )
+        self.assertEqual(line_product_pack_component_2_data["qty_ordered"], 2)
+        self.assertEqual(line_product_pack_component_2_data["qty_done"], 2)
